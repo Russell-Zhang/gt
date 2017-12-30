206f56851992f0f4938466a99d57e6c1b0bbc1a5
russellzhang <russell.zh@gmail.com>
2017-12-30 12:19:42 -0800

    11th commit

--- gt_rn.py
+++ gt_rn.py
@@ -2,3 +2,4 @@
 7th edit
 8th edit from local repo
 9th edit for gui
+11th edit
--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 read -p "Type something and hit enter: " msg
 echo You typed [ $msg ]
 echo Exiting, byebye.
+
+#comment for 11th_commit
